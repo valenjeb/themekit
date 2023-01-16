7117e3b82a005d0aaf2d2305027a563207080c75
Valentin Jebelev <vala.jebelev@gmail.com>
2023-01-16 15:27:21 +0200

    Add faceded object public methods

--- src/Facades/Engine.php
+++ src/Facades/Engine.php
@@ -5,10 +5,17 @@ declare(strict_types=1);
 namespace Devly\ThemeKit\Facades;
 
 use Devly\ThemeKit\Bridges\Latte\LatteEngine;
+use Devly\ThemeKit\UI\Finder;
+use Latte\Macro;
 
 /**
- * @method static void render(string $name, $params = [], ?string $block = null)
- * @method static string renderToString(string $name, $params = [], ?string $block = null)
+ * @method static void render(string $name, $params = [], ?string $block = null) Renders template to output.
+ * @method static string renderToString(string $name, $params = [], ?string $block = null) Renders template to output.
+ * @method static Finder getFinder Return an instance template file finder.
+ * @method static LatteEngine addFilter(?string $name, callable $callback) Registers run-time filter.
+ * @method static LatteEngine addFilterLoader(callable $callback) Registers filter loader.
+ * @method static LatteEngine addFunction(string $name, callable $callback) Registers run-time function.
+ * @method static LatteEngine addMacro(string $name, Macro $macro) Adds new macro.
  */
 class Engine extends Facade
 {
--- src/Facades/Theme.php
+++ src/Facades/Theme.php
@@ -4,8 +4,30 @@ declare(strict_types=1);
 
 namespace Devly\ThemeKit\Facades;
 
+use WP_Theme;
+
 /**
- * @method static mixed getOption(string $key, $default = null)
+ * @method static mixed getOption(string $key, mixed $default = null)
+ * @method static bool setOption(string $name, mixed $value)
+ * @method static bool deleteOption(string $name)
+ * @method static array getOptions
+ * @method static bool deleteOptions
+ * @method static bool importOptions(string|\Devly\WP\Models\Theme $theme)
+ * @method static array<string, mixed>|false|string display(string $header, bool $markup = true, bool $translate = true)
+ * @method static array<string, mixed>|false|string get(string $header) Gets a raw, un-formatted theme header.
+ * @method static string getName(bool $formatted = false) Retrieves the theme name
+ * @method static string getDescription(bool $formatted = false) Retrieves the theme description
+ * @method static string getVersion Retrieves the theme version
+ * @method static string getTextDomain Retrieves the theme text domain
+ * @method static string getSlug Returns the directory name of the theme's "stylesheet" files, inside the theme root.
+ * @method static string getUrl Returns the URL to the directory of a theme's “stylesheet” files.
+ * @method static string getPath Returns the absolute path to the directory of the theme's “stylesheet” files.
+ * @method static string getTemplate
+ * @method static string|null getScreenshot(bool $absolute = true)
+ * @method static bool isActive
+ * @method static bool hasParent
+ * @method static \Devly\WP\Models\Theme getParent
+ * @method static WP_Theme getCoreObject
  */
 class Theme extends Facade
 {
