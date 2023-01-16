a8fc3ec01e26ec17f9ab50dc124e3a8028ad465c
Valentin Jebelev <vala.jebelev@gmail.com>
2023-01-16 13:13:03 +0200

    Fix presenter namespace config key

--- src/Bridges/Routing/RoutingServiceProvider.php
+++ src/Bridges/Routing/RoutingServiceProvider.php
@@ -34,7 +34,7 @@ class RoutingServiceProvider extends ServiceProvider implements IBootableService
         add_filter(Hooks::FILTER_CONTROLLER_SUFFIX, static fn () => 'Presenter');
         add_filter(Hooks::FILTER_DEFAULT_CONTROLLER, static fn () => DefaultPresenter::class);
         add_filter(Hooks::FILTER_NAMESPACE, static fn () => $di->config(
-            'view.namespace',
+            'view.namespace.presenter',
             $di->config('app.namespace', 'App\\') . 'UI\\Presenters'
         ));
 
