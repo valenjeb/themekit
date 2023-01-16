6ca8a4a447d178124889d1d526b9e322fe41f211
Valentin Jebelev <vala.jebelev@gmail.com>
2023-01-16 12:57:18 +0200

    Replace functions logic with namespaced function

--- src/globals.php
+++ src/globals.php
@@ -5,37 +5,41 @@
 declare(strict_types=1);
 
 use Devly\ThemeKit\Application;
-use Devly\ThemeKit\Facades\App;
-use Devly\ThemeKit\Facades\Bundle;
-use Devly\ThemeKit\Facades\Engine;
-use Devly\ThemeKit\Facades\Mix;
 use Devly\ThemeKit\UI\Contracts\ITemplate;
 use Devly\WP\Assets\Asset;
+use Devly\WP\Assets\Bundle;
 use Devly\WP\Assets\Manager;
 
 if (! function_exists('app')) {
-    /** @return Application|mixed */
+    /**
+     * Retrieves an instance of current application object
+     * or a service from the application container.
+     *
+     * @return Application|mixed
+     */
     function app(?string $key = null)
     {
-        if ($key) {
-            return App::get($key);
-        }
-
-        return App::getInstance();
+        return \Devly\ThemeKit\app($key);
     }
 }
 
 if (! function_exists('mix')) {
+    /**
+     * Get an asset from the assets manifest as an Asset object
+     */
     function mix(string $path): Asset
     {
-        return Mix::get($path);
+        return \Devly\ThemeKit\mix($path);
     }
 }
 
 if (! function_exists('asset')) {
+    /**
+     * Get an asset from the assets manifest as an Asset object
+     */
     function asset(string $path): Asset
     {
-        return Mix::get($path);
+        return \Devly\ThemeKit\asset($path);
     }
 }
 
@@ -43,31 +47,23 @@ if (! function_exists('bundle')) {
     /**
      * Retrieve an assets bundle by name
      *
-     * @return \Devly\WP\Assets\Bundle|Manager
+     * @return Bundle|Manager
      */
-    function bundle(string $name, ?\Devly\WP\Assets\Bundle $bundle = null)
+    function bundle(string $name, ?Bundle $bundle = null)
     {
-        if ($bundle) {
-            return Bundle::add($name, $bundle);
-        }
-
-        return Bundle::get($name);
+        return \Devly\ThemeKit\bundle($name, $bundle);
     }
 }
 
 if (! function_exists('view')) {
     /**
+     * Renders view to output
+     *
      * @param ITemplate|string            $template A template file path or a Template object
      * @param array<string, mixed>|object $params   A list of parameters to pass to the template
      */
     function view($template, $params = []): void
     {
-        if ($template instanceof ITemplate) {
-            $template->render(null, (array) $params);
-
-            return;
-        }
-
-        Engine::render($template, $params);
+        \Devly\ThemeKit\view($template, $params);
     }
 }
