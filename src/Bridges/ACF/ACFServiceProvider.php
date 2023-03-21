ebbb07758ac84a6d55489fcf3d76b56a02d1af6b
Valentin Jebelev <vala.jebelev@gmail.com>
2023-03-21 14:35:29 +0200

    Add support for setting ACF field data programmatically using $object->setField()

--- src/Bridges/ACF/ACFServiceProvider.php
+++ src/Bridges/ACF/ACFServiceProvider.php
@@ -6,15 +6,22 @@ namespace Devly\ThemeKit\Bridges\ACF;
 
 use Devly\DI\Contracts\IBootableServiceProvider;
 use Devly\WP\Models\Filter;
+use Devly\WP\Models\Post;
+use Devly\WP\Models\Term;
 
 use function function_exists;
+use function sprintf;
 
 class ACFServiceProvider implements IBootableServiceProvider
 {
     public function boot(): void
     {
         add_filter(Filter::POST_PRE_GET_META_FIELD, [$this, 'getField'], 0, 3);
+        add_filter(Filter::POST_PRE_SET_META_FIELD, [$this, 'setField'], 0, 5);
         add_filter(Filter::SITE_PRE_GET_OPTION, [$this, 'getOption'], 0, 2);
+        add_filter(Filter::SITE_PRE_SET_OPTION, [$this, 'setOption'], 0, 3);
+        add_filter(Filter::TERM_PRE_GET_META_FIELD, [$this, 'getTermField'], 0, 3);
+        add_filter(Filter::TERM_PRE_SET_META_FIELD, [$this, 'setTermField'], 0, 5);
     }
 
     /**
@@ -33,6 +40,50 @@ class ACFServiceProvider implements IBootableServiceProvider
 
     /**
      * @param mixed $value
+     * @param mixed $previous
+     */
+    public function setField(?bool $result, string $key, $value, $previous, Post $postObject): ?bool
+    {
+        if (! function_exists('update_field')) {
+            return $result;
+        }
+
+        return update_field($key, $value, $postObject->ID);
+    }
+
+    /**
+     * @param mixed $value
+     *
+     * @return mixed
+     */
+    public function getTermField($value, string $key, Term $object)
+    {
+        if (! function_exists('get_field')) {
+            return $value;
+        }
+
+        $termID = sprintf('%s_%s', $object->getCoreObject()->taxonomy, $object->ID);
+
+        return get_field($key, $termID) ?? $value;
+    }
+
+    /**
+     * @param mixed $value
+     * @param mixed $previous
+     */
+    public function setTermField(?bool $result, string $key, $value, $previous, Term $object): ?bool
+    {
+        if (! function_exists('update_field')) {
+            return $result;
+        }
+
+        $termID = sprintf('%s_%s', $object->getCoreObject()->taxonomy, $object->ID);
+
+        return update_field($key, $value, $termID);
+    }
+
+    /**
+     * @param mixed $value
      *
      * @return mixed
      */
@@ -43,5 +94,15 @@ class ACFServiceProvider implements IBootableServiceProvider
         }
 
         return get_field($key, 'option') ?? $value;
+    }
+
+    /** @param mixed $value */
+    public function setOption(?bool $result, string $key, $value): ?bool
+    {
+        if (! function_exists('update_field')) {
+            return $result;
+        }
+
+        return update_field($key, $value, 'option');
     }
 }
